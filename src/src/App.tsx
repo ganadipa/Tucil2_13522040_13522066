0c136347d7c4c76d340ae6ad5111a554d9408060
Nyoman-Gana <nyomanganadipa@gmail.com>
2024-03-13 04:56:55 +0700

    Add header

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -2,8 +2,10 @@ import PageRoute from "./routes/pageroute";
 
 function App() {
   return (
-    <div className="App h-[100vh] w-[100vw] bg-sky-950 ">
-      <header>Bezier Curve (middle point divide and conquer)</header>
+    <div className="App h-[100vh] w-[100vw] bg-sky-950 flex flex-col">
+      <header className="place-self-center">
+        Bezier Curve (middle point divide and conquer)
+      </header>
       <div className="py-8">
         <PageRoute />
       </div>
